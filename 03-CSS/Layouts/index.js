fc85c9c5c5bd72db4159a42e9f0d98bee5766226
Valentine Kalchevski <valentin.kalchevsky@yellow.systems>
2022-06-01 12:27:53 +0300

    css block

---
+++ 03-CSS/Layouts/index.js
@@ -0,0 +1,3 @@
+//TODO https://www.freecodecamp.org/learn/responsive-web-design/#css-flexbox
+//TODO https://www.freecodecamp.org/learn/responsive-web-design/#css-grid
+//TODO https://www.youtube.com/watch?v=FNsn0pBmrKs
---
+++ 03-CSS/Responsive/index.js
@@ -0,0 +1,3 @@
+//TODO https://developer.mozilla.org/ru/docs/Web/CSS/Media_Queries/Using_media_queries
+
+//TODO https://www.w3.org/TR/mediaqueries-3/
