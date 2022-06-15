4ef136f40b89355ada73079f87123e8be65b6fd0
Ilia Prihodko <47753668+ilia78568@users.noreply.github.com>
2022-06-15 12:27:43 +0300

    Update index.js

--- 02-Html/Accessibillity/index.js
+++ 02-Html/Accessibillity/index.js
@@ -1,1 +1,3 @@
 // TODO https://developer.mozilla.org/ru/docs/Learn/Accessibility/What_is_accessibility
+// TODO https://developer.mozilla.org/ru/docs/Learn/Accessibility/HTML
+// TODO https://developer.mozilla.org/ru/docs/Learn/Accessibility/CSS_and_JavaScript
