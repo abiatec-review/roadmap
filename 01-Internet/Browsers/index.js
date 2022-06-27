003fecf0d9df2ab6abed3a2e81061a51f5e76077
Ilia Prihodko <47753668+ilia78568@users.noreply.github.com>
2022-06-27 12:52:38 +0300

    Update index.js

--- 01-Internet/Browsers/index.js
+++ 01-Internet/Browsers/index.js
@@ -1,5 +1,6 @@
 // Ru sources
 // TODO https://www.html5rocks.com/ru/tutorials/internals/howbrowserswork/
+// TODO https://habr.com/en/post/484900/
 
 // Eng sources:
 // TODO https://www.browserstack.com/guide/browser-rendering-engine
