020baf7599945c95035fb04c16307964144301b8
Valentine Kalchevski <valentin.kalchevsky@yellow.systems>
2022-06-28 17:37:25 +0300

    initial knowledge

---
+++ initialKnowledge.js
@@ -0,0 +1,19 @@
+//remove 02-Html/Accessibillity/index.js
+//remove 02-Html/Convinient/index.js
+//remove 02-Html/Semantic/index.js
+//remove 02-Html/Seo/index.js
+//remove 03-CSS/Basics/index.js
+//remove 03-CSS/Css-architecture/BEM/index.js
+//remove 03-CSS/Css-frameworks/Material-ui/index.js
+//remove 03-CSS/Css-frameworks/Reactstrap/index.js
+//remove 03-CSS/Css-prepros/Sass/index.js
+//remove 03-CSS/Css-prepros/index.js
+//remove 03-CSS/Layouts/index.js
+//remove 03-CSS/ModernCSS/CSS Modules/index.js
+//remove 03-CSS/ModernCSS/Styled JSX/index.js
+//remove 03-CSS/Responsive/index.js
+//remove 04-VCS/Basics/index.js
+//remove 04-VCS/Repo-hostings/Bitbucket/index.js
+//remove 06-Build-tools/Code-formatters/eslint/index.js
+//remove 06-Build-tools/Code-formatters/prettier/index.js
+//remove 06-Build-tools/Module-Bundlers/parcel/index.js
