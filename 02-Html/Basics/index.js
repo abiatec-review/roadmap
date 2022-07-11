331a7d647bdc8a45ee41c252ce9e6f46d95bbaa5
abiatec <102151286+abiatec-review@users.noreply.github.com>
2022-07-11 10:52:14 +0300

    Update index.js

--- 02-Html/Basics/index.js
+++ 02-Html/Basics/index.js
@@ -1,3 +1,3 @@
 //TODO HTML tables https://developer.mozilla.org/ru/docs/Learn/HTML/Tables/Basics
 //TODO HTML tables https://developer.mozilla.org/ru/docs/Learn/HTML/Tables/Advanced
-//TODO HTML tables https://frontendmasters.com/courses/html-email-v2/
+//TODO https://www.youtube.com/watch?v=TNCaCDFKJfc&ab_channel=MaxGraph-c%D0%B0%D0%B9%D1%82%D1%8B%D0%BA%D0%B0%D0%BA%D1%81%D1%82%D1%80%D0%B0%D1%81%D1%82%D1%8C
