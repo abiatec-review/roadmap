246f02c468bc54135fe8b3a9393125bbee8e06b8
ilia78568 <ilia19.99@mail.ru>
2022-07-29 12:19:49 +0300

    add gh-pages link

--- 01-Internet/test.js
+++ 01-Internet/test.js
@@ -4,3 +4,5 @@
 // Расшифровать модули url адреса https://www.abiatec.by:80/home/desktop/index.html?id=1&name=student
 // Захостить react app со своим Именем и фамилией в тэге h1 на хероку & ghpages
 // Разница между http и https
+
+// host gh-pages https://ilia78568.github.io/Forms/
