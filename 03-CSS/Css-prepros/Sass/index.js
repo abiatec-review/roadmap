a54a1fd6726e423c406f0089d86a0ef680241cbd
yuriy17 <yuriy_sh@zoho.com>
2022-12-26 15:31:28 +0300

    interview [00, 01, 03] added initial knowledge

--- 03-CSS/Css-prepros/Sass/index.js
+++
@@ -1,8 +0,0 @@
-//TODO https://sass-scss.ru/documentation/
-
-//TODO https://sass-scss.ru/documentation/rasshirenie_css/vlozhennie_pravila/
-//TODO https://sass-scss.ru/documentation/rasshirenie_css/ssilka_na_roditelya_selektora/
-//TODO https://sass-scss.ru/documentation/rasshirenie_css/vlozhennie_svoistva/
-//TODO https://sass-scss.ru/documentation/sassscript/peremennie/
-//TODO https://sass-scss.ru/documentation/sassscript/mapi/
-//TODO https://sass-scss.ru/documentation/sassscript/interpolyatsiya/
--- 03-CSS/ModernCSS/CSS Modules/index.js
+++
@@ -1,3 +0,0 @@
-//TODO https://css-tricks.com/css-modules-part-1-need/
-//TODO https://css-tricks.com/css-modules-part-2-getting-started/
-//TODO https://css-tricks.com/css-modules-part-3-react/
