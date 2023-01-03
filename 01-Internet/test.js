64a04fbc93ab88a18fc2dcdb1c226b3a37cd6be6
yuriy17 <yuriy_sh@zoho.com>
2023-01-03 12:47:02 +0300

    interview [00, 01] added initial knowledge

--- 01-Internet/test.js
+++ 01-Internet/test.js
@@ -3,4 +3,3 @@
 // Как работает dns запрос
 // Расшифровать модули url адреса https://www.abiatec.by:80/home/desktop/index.html?id=1&name=student
 // Захостить react app со своим Именем и фамилией в тэге h1 на хероку & ghpages
-// Разница между http и https
