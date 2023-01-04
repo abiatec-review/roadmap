78d420ae25cc6e5e15d27b4a7b4e2839361c9dfd
yuriy17 <yuriy_sh@zoho.com>
2023-01-04 13:56:02 +0300

    added general-knowledge

--- 00-general-knowledge/index.js
+++ 00-general-knowledge/index.js
@@ -1,11 +1,4 @@
-// Что такое прогрессивный рендеринг? TODO https://www.youtube.com/watch?v=IooJ3P2VUYs&t=26s
-// Разница между Progressive Enhancement и Graceful Degradation TODO https://www.youtube.com/watch?v=rlWgI7AvV18&t=286s
 // Что такое кроссбраузерность? //TODO https://www.youtube.com/watch?v=kx3dR6ztICU&t=90s
-// Что такое Babel? Для чего он используется? // TODO https://youtu.be/w-vUj0gHGgg?t=681
-// Способы уменьшения времени загрузки веб-страницы //TODO https://www.youtube.com/watch?v=nvktMVFM0_M&t=678s
 // Особенности разработки мультиязычных сайтов? //TODO https://www.youtube.com/watch?v=yvOXvZ8aEFo&t=82s
-// Что такое Progressive Web Application? TODO https://www.youtube.com/watch?v=XtQPrt8G0n8&t=76s
 // Что Такое API? TODO https://www.youtube.com/watch?v=ngyOYuTrUk8&t=98s
-// Что такое REST? TODO https://www.youtube.com/watch?v=ovV8GhIkzBE&t=352s
 // Что такое IP-адрес? TODO https://www.youtube.com/watch?v=70VnuTXi4Wk&t=720s
-// Разница между URI и URL? TODO https://www.youtube.com/watch?v=70VnuTXi4Wk&t=844s
--- initial-knowledge.js
+++ initial-knowledge.js
@@ -1,7 +1,15 @@
 /** 
  * 00 
  * 
+ * Что такое прогрессивный рендеринг? TODO https://www.youtube.com/watch?v=IooJ3P2VUYs&t=26s
+ * Разница между Progressive Enhancement и Graceful Degradation TODO https://www.youtube.com/watch?v=rlWgI7AvV18&t=286s
+ * Что такое Babel? Для чего он используется? // TODO https://youtu.be/w-vUj0gHGgg?t=681
+ * Способы уменьшения времени загрузки веб-страницы //TODO https://www.youtube.com/watch?v=nvktMVFM0_M&t=678s
+ * Что такое Progressive Web Application? TODO https://www.youtube.com/watch?v=XtQPrt8G0n8&t=76s
+ * Что такое REST? TODO https://www.youtube.com/watch?v=ovV8GhIkzBE&t=352s
+ * Разница между URI и URL? TODO https://www.youtube.com/watch?v=70VnuTXi4Wk&t=844s
  * респонсив и адаптив
+ * 
  * CDN 
  */
 
