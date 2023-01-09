0057e306d28a835e7fe0a0f66b5da821aeacb386
yuriy17 <yuriy_sh@zoho.com>
2023-01-09 16:50:18 +0300

    added 00, 02, 14 topics

---
+++ 14-Javascript/concepts/algorithms/index.js
@@ -0,0 +1,27 @@
+// TODO realize algorithms on js
+// 1. https://github.com/Shastel/towel-sort
+// 2. https://github.com/romacher/morse-decoder
+
+/** Additional tasks on codewars
+ * 
+ * 7 kyu https://www.codewars.com/kata/count-strings-in-objects
+ * 6 kyu https://www.codewars.com/kata/base-conversion
+ * 6 kyu https://www.codewars.com/kata/bingo-card
+ * 6 kyu https://www.codewars.com/kata/bit-counting
+ * 6 kyu https://www.codewars.com/kata/clocky-mc-clock-face
+ * 6 kyu https://www.codewars.com/kata/is-a-number-prime
+ * 6 kyu https://www.codewars.com/kata/lottery-ticket
+ * 6 kyu https://www.codewars.com/kata/weird-string-case
+ * 6 kyu https://www.codewars.com/kata/who-likes-it
+ * 5 kyu https://www.codewars.com/kata/rgb-to-hex-conversion
+ */
+
+
+/** Reading 
+ * 
+ * https://habr.com/ru/post/359192/  and repository itself https://github.com/trekhleb/javascript-algorithms
+ * 
+ * Complexity of algorithms estimation, or What is O(log n)
+ * 
+ * https://tproger.ru/articles/computational-complexity-explained/
+ */
