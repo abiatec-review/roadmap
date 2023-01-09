0d397bdb9762d23dbac780435baa3282edd9d9ad
yuriy17 <yuriy_sh@zoho.com>
2023-01-09 16:50:18 +0300

    added 00, 02, 14 topics

--- 02-Html/Basics/index.js
+++ 02-Html/Basics/index.js
@@ -1,3 +1,13 @@
 //TODO HTML tables https://developer.mozilla.org/ru/docs/Learn/HTML/Tables/Basics
 //TODO HTML tables https://developer.mozilla.org/ru/docs/Learn/HTML/Tables/Advanced
 //TODO https://www.youtube.com/watch?v=TNCaCDFKJfc&ab_channel=MaxGraph-c%D0%B0%D0%B9%D1%82%D1%8B%D0%BA%D0%B0%D0%BA%D1%81%D1%82%D1%80%D0%B0%D1%81%D1%82%D1%8C
+
+/** Complete Guide to Responsive Images
+ * 
+ * https://abatickaya.medium.com/responsive-imgs-998f103a7a32
+ */
+
+/** Email's layout rules
+ * 
+ * https://www.unisender.com/ru/blog/sovety/kak-sverstat-pismo-instruktsiya-dlya-chaynikov/
+ */
